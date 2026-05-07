bdfe2ec683ddfce04f5543a662705d0c74247c5a
m_skvortsov <m_skvortsov@wargaming.net>
2026-05-07 17:38:09 +0300

    feat: prove szpilrajn_theorem via ZornsLemma

--- posets/dimension/Szpilrajn.v
+++ posets/dimension/Szpilrajn.v
@@ -40,3 +40,89 @@ Proof.
     + exfalso; apply Hinc; left; eapply poset_trans; [exact Hpb | exact Hbq].
   - intros a b c Hab Hbc; eapply t_trans; eauto.
 Qed.
+
+(** SubRel is antisymmetric under propositional + functional extensionality. *)
+Lemma SubRel_antisym : forall (A : Type) (P Q : A -> A -> Prop),
+  (forall x y, P x y -> Q x y) ->
+  (forall x y, Q x y -> P x y) ->
+  P = Q.
+Proof.
+  intros A P Q HPQ HQP.
+  apply functional_extensionality; intro x.
+  apply functional_extensionality; intro y.
+  apply propositional_extensionality; split; auto.
+Qed.
+
+Theorem szpilrajn_theorem :
+  forall (A : Type) (R : A -> A -> Prop) `{HR : IsPoset A R},
+  exists L : A -> A -> Prop,
+    IsPoset A L /\
+    (forall x y, L x y \/ L y x) /\
+    (forall x y, R x y -> L x y).
+Proof.
+  intros A R HR.
+  (* Work over the sigma type of poset-extensions of R. *)
+  set (Ext := { P : A -> A -> Prop | IsPoset A P /\ forall x y, R x y -> P x y }).
+  set (ExtOrd := fun s1 s2 : Ext =>
+    forall x y, proj1_sig s1 x y -> proj1_sig s2 x y).
+
+  (* ExtOrd is a partial order on Ext. *)
+  assert (ExtOrd_order : order ExtOrd).
+  { constructor; unfold ExtOrd, reflexive, transitive, antisymmetric.
+    - auto.
+    - eauto.
+    - intros [P HP] [Q HQ] H12 H21.
+      apply subset_eq_compat; simpl in *.
+      apply SubRel_antisym; auto. }
+
+  (* Every chain in (Ext, ExtOrd) has an upper bound. *)
+  assert (ExtOrd_ub : forall C : Ensemble Ext, chain ExtOrd C ->
+    exists ub : Ext, forall s : Ext, In C s -> ExtOrd s ub).
+  { intros C HC.
+    destruct (classic (Inhabited Ext C)) as [[s0 Hs0] | Hempty].
+    - (* Non-empty: take the union. *)
+      set (union_rel := fun x y => exists s : Ext, In C s /\ proj1_sig s x y).
+      assert (union_poset : IsPoset A union_rel).
+      { constructor.
+        - intro x. exists s0. split; [exact Hs0 | apply (proj1 (proj2_sig s0)).(poset_refl)].
+        - intros x y [s1 [Hs1 H1]] [s2 [Hs2 H2]].
+          destruct (HC s1 s2 Hs1 Hs2) as [H12 | H21].
+          + eapply (proj1 (proj2_sig s2)).(poset_antisym); eauto.
+          + eapply (proj1 (proj2_sig s1)).(poset_antisym); eauto.
+        - intros x y z [s1 [Hs1 H1]] [s2 [Hs2 H2]].
+          destruct (HC s1 s2 Hs1 Hs2) as [H12 | H21].
+          + exists s2. split; [exact Hs2 | eapply (proj1 (proj2_sig s2)).(poset_trans); [exact (H12 x y H1) | exact H2]].
+          + exists s1. split; [exact Hs1 | eapply (proj1 (proj2_sig s1)).(poset_trans); [exact H1 | exact (H21 y z H2)]]. }
+      assert (union_ext : forall x y, R x y -> union_rel x y).
+      { intros x y HR'. exists s0. split; [exact Hs0 | exact (proj2 (proj2_sig s0) x y HR')]. }
+      exists (exist _ union_rel (conj union_poset union_ext)).
+      intros [P [HP Hext]] HPC. unfold ExtOrd; simpl.
+      intros x y HPxy. exact (ex_intro _ (exist _ P (conj HP Hext)) (conj HPC HPxy)).
+    - (* Empty: R itself is an upper bound vacuously. *)
+      exists (exist _ R (conj HR (fun x y h => h))).
+      intros s Hs. exfalso. apply Hempty. exists s. exact Hs. }
+
+  (* Apply Zorn's lemma. *)
+  destruct (ZornsLemma ExtOrd ExtOrd_order ExtOrd_ub) as [[M [HM_poset HM_ext]] HM_max].
+
+  (* M is total: if not, the extension TC(M ∪ {(y,x)}) contradicts maximality. *)
+  assert (M_total : forall x y, M x y \/ M y x).
+  { intros x y.
+    destruct (classic (M x y \/ M y x)) as [? | Hinc]; [auto |].
+    exfalso.
+    set (ext_step := fun a b => M a b \/ (a = y /\ b = x)).
+    set (M' := @clos_trans A ext_step).
+    assert (HM'_poset : IsPoset A M') by
+      (apply add_incomparable_general; auto).
+    assert (HM'_ext : forall a b, R a b -> M' a b).
+    { intros a b Hab. apply t_step. left. exact (HM_ext a b Hab). }
+    set (s_M' := exist _ M' (conj HM'_poset HM'_ext) : Ext).
+    assert (HMM' : ExtOrd (exist _ M (conj HM_poset HM_ext)) s_M').
+    { unfold ExtOrd, s_M'; simpl. intros a b Hmab. apply t_step. left. exact Hmab. }
+    pose proof (HM_max s_M' HMM') as Heq.
+    assert (HMeqM' : M = M') by exact (f_equal (@proj1_sig _ _) Heq).
+    apply Hinc. right.
+    rewrite HMeqM'. apply t_step. right. auto. }
+
+  exact (ex_intro _ M (conj HM_poset (conj M_total HM_ext))).
+Qed.
