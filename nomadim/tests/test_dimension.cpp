a557aa672323b9eae1fd8895e3ce53428c5a85f2
m_skvortsov <m_skvortsov@wargaming.net>
2026-06-01 19:33:08 +0300

    test(nomadim): cover have_cycle/is_bipartite/check_if_critical/find_critical_pairs

--- nomadim/tests/test_dimension.cpp
+++ nomadim/tests/test_dimension.cpp
@@ -3,6 +3,7 @@
 #include "nomadim/process_graph.hpp"
 #include "nomadim/execution.hpp"
 #include "nomadim/types.hpp"
+#include "nomadim/floyd.hpp"
 
 using namespace nomadim;
 
@@ -50,4 +51,61 @@ TEST(Dimension, StandardExampleS3IsNotDim2) {
     s3[1] = {3, 5};
     s3[2] = {3, 4};
     EXPECT_FALSE(is_dim2(s3));
+}
+
+TEST(Dimension, HaveCycleEdgeCases) {
+    EXPECT_FALSE(have_cycle(adjacency_list{}));
+    EXPECT_FALSE(have_cycle(adjacency_list{{}}));
+    EXPECT_TRUE(have_cycle(adjacency_list{{0}}));
+    EXPECT_TRUE(have_cycle(adjacency_list{{1},{0}}));
+    EXPECT_FALSE(have_cycle(adjacency_list{{1},{2},{}}));
+}
+
+TEST(Dimension, IsBipartiteEdgeCases) {
+    EXPECT_TRUE(is_bipartite(adjacency_list{}));
+    EXPECT_TRUE(is_bipartite(adjacency_list{{}}));
+    EXPECT_TRUE(is_bipartite(adjacency_list{{1,3},{0,2},{1,3},{0,2}}));
+    EXPECT_FALSE(is_bipartite(adjacency_list{{1,2},{0,2},{0,1}}));
+}
+
+TEST(Dimension, CheckIfCriticalOnSmallPoset) {
+    adjacency_list g = {{}, {2}, {}};
+    std::vector<int> m = make_graph_matrix(g);
+    floyd(m.data(), 3);
+    EXPECT_FALSE(check_if_critical(m.data(), 3, 0, 1));
+    EXPECT_TRUE(check_if_critical(m.data(), 3, 1, 0));
+}
+
+TEST(Dimension, FindCriticalPairsCounts) {
+    {
+        ProcessGraph g = ProcessGraph::build(Execution{4, {{0,1},{1,2},{2,3},{0,2}}});
+        std::vector<int> m = make_graph_matrix(g.graph);
+        EXPECT_EQ(find_critical_pairs(m.data(), (int)g.graph.size()).size(), 25u);
+    }
+    {
+        adjacency_list s3(6); s3[0] = {4,5}; s3[1] = {3,5}; s3[2] = {3,4};
+        std::vector<int> m = make_graph_matrix(s3);
+        EXPECT_EQ(find_critical_pairs(m.data(), 6).size(), 3u);
+    }
+    {
+        adjacency_list chain = {{1},{2},{3},{}};
+        std::vector<int> m = make_graph_matrix(chain);
+        EXPECT_EQ(find_critical_pairs(m.data(), 4).size(), 0u);
+    }
+    {
+        adjacency_list a3(3);
+        std::vector<int> m = make_graph_matrix(a3);
+        EXPECT_EQ(find_critical_pairs(m.data(), 3).size(), 6u);
+    }
+}
+
+TEST(Dimension, CheckCriticalPairsGraphBipartiteness) {
+    adjacency_list a3(3);
+    std::vector<int> m = make_graph_matrix(a3);
+    auto cps = find_critical_pairs(m.data(), 3);
+    EXPECT_TRUE(check_critical_pairs_graph(a3, cps));
+    adjacency_list s3(6); s3[0] = {4,5}; s3[1] = {3,5}; s3[2] = {3,4};
+    std::vector<int> ms = make_graph_matrix(s3);
+    auto cps3 = find_critical_pairs(ms.data(), 6);
+    EXPECT_FALSE(check_critical_pairs_graph(s3, cps3));
 }
