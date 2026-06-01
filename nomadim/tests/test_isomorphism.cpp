11a027929d3ec78b3a0ac12356130e801469ba2f
m_skvortsov <m_skvortsov@wargaming.net>
2026-06-01 19:35:30 +0300

    test(nomadim): cover canonical_sync_name and is_isomorphic edge cases

--- nomadim/tests/test_isomorphism.cpp
+++ nomadim/tests/test_isomorphism.cpp
@@ -2,6 +2,7 @@
 #include "nomadim/isomorphism.hpp"
 #include "nomadim/process_graph.hpp"
 #include "nomadim/execution.hpp"
+#include <algorithm>
 
 using namespace nomadim;
 
@@ -25,4 +26,33 @@ TEST(Isomorphism, GenerateAllIsomorphicCardinality) {
     ProcessGraph a = ProcessGraph::build(Execution{3, {{0,1}}});
     auto all = generate_all_isomorphic(a);
     EXPECT_EQ(all.size(), 6u); // 3! permutations
+}
+
+TEST(Isomorphism, CanonicalSyncNamePermutationInvariant) {
+    ProcessGraph a = ProcessGraph::build(Execution{4, {{0,1}}});
+    ProcessGraph b = ProcessGraph::build(Execution{4, {{2,3}}});
+    EXPECT_EQ(canonical_sync_name(a), canonical_sync_name(b));
+}
+
+TEST(Isomorphism, CanonicalSyncNameSortedAndDistinguishes) {
+    ProcessGraph a = ProcessGraph::build(Execution{4, {{0,1}}});
+    auto key = canonical_sync_name(a);
+    EXPECT_TRUE(std::is_sorted(key.begin(), key.end()));
+    ProcessGraph path = ProcessGraph::build(Execution{4, {{0,1},{1,2},{2,3}}});
+    ProcessGraph star = ProcessGraph::build(Execution{4, {{0,1},{0,2},{0,3}}});
+    EXPECT_NE(canonical_sync_name(path), canonical_sync_name(star));
+}
+
+TEST(Isomorphism, GenerateAllIsomorphicCardinalityN4) {
+    ProcessGraph a = ProcessGraph::build(Execution{4, {{0,1}}});
+    EXPECT_EQ(generate_all_isomorphic(a).size(), 24u);
+}
+
+TEST(Isomorphism, NotIsomorphicWhenShapeDiffers) {
+    ProcessGraph a = ProcessGraph::build(Execution{3, {{0,1}}});
+    ProcessGraph diff_procs = ProcessGraph::build(Execution{4, {{0,1}}});
+    EXPECT_FALSE(is_isomorphic(a, diff_procs));
+    ProcessGraph more_syncs = ProcessGraph::build(Execution{3, {{0,1},{1,2}}});
+    EXPECT_FALSE(is_isomorphic(a, more_syncs));
+    EXPECT_TRUE(is_isomorphic(a, a));
 }
