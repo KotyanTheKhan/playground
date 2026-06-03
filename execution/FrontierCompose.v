682d98ae1dfe1917bb117ab5d669fffaedaa976b
m_skvortsov <m_skvortsov@wargaming.net>
2026-06-03 13:54:15 +0300

    feat(frontier-compose): crown3_dim_ge_3 (S3 frontier raises dimension to >= 3)

--- execution/FrontierCompose.v
+++ execution/FrontierCompose.v
@@ -207,3 +207,155 @@ Proof.
   destruct Hcard as [nc [Hnc Hnc2]].
   pose proof (dimension_is_minimum Hdim RS nc HReal Hnc) as Hle. lia.
 Qed.
+
+(* ===================================================================== *)
+(* NEGATIVE direction: the S3 crown frontier (i <> j) forces dim >= 3.    *)
+(* ===================================================================== *)
+
+Definition crownF (a b : Fin.t 3) : Prop := a <> b.
+Definition crown3 : Carrier (Fin.t 3) (Fin.t 3) -> Carrier (Fin.t 3) (Fin.t 3) -> Prop
+  := compose_le crownF.
+
+#[export] Instance crown3_IsPoset : IsPoset (Carrier (Fin.t 3) (Fin.t 3)) crown3
+  := compose_IsPoset crownF.
+
+(* The three concrete elements of [Fin.t 3]. *)
+Definition c0 : Fin.t 3 := Fin.F1.
+Definition c1 : Fin.t 3 := Fin.FS Fin.F1.
+Definition c2 : Fin.t 3 := Fin.FS (Fin.FS Fin.F1).
+
+Lemma fin3_cases : forall i : Fin.t 3, i = c0 \/ i = c1 \/ i = c2.
+Proof.
+  intro i.
+  apply (Fin.caseS' i); [ left; reflexivity | intro p ].
+  apply (Fin.caseS' p); [ right; left; reflexivity | intro q ].
+  apply (Fin.caseS' q); [ right; right; reflexivity | intro r ].
+  exact (Fin.case0 (fun _ => _) r).
+Qed.
+
+(* The six carrier elements; the carrier is finite. *)
+Lemma crown_carrier_finite :
+  Finite (Carrier (Fin.t 3) (Fin.t 3)) (Full_set (Carrier (Fin.t 3) (Fin.t 3))).
+Proof.
+  set (E := Carrier (Fin.t 3) (Fin.t 3)).
+  set (six :=
+    Add E (Add E (Add E (Add E (Add E (Add E (Empty_set E)
+      (inA c0)) (inA c1)) (inA c2)) (inB c0)) (inB c1)) (inB c2)).
+  assert (Heq : Full_set E = six).
+  { apply Extensionality_Ensembles; split.
+    - intros x _. unfold six, E.
+      destruct x as [a|b].
+      + destruct (fin3_cases a) as [-> | [-> | ->]].
+        * left; left; left; left; left; right; constructor. (* inA c0: layer1, 5 left *)
+        * left; left; left; left; right; constructor.       (* inA c1: layer2, 4 left *)
+        * left; left; left; right; constructor.             (* inA c2: layer3, 3 left *)
+      + destruct (fin3_cases b) as [-> | [-> | ->]].
+        * left; left; right; constructor.                   (* inB c0: layer4, 2 left *)
+        * left; right; constructor.                         (* inB c1: layer5, 1 left *)
+        * right; constructor.                               (* inB c2: layer6, 0 left *)
+    - intros x _. constructor. }
+  rewrite Heq. unfold six.
+  repeat (apply Add_preserves_Finite). apply Empty_is_finite.
+Qed.
+
+Lemma crown_critical : forall i : Fin.t 3, IsCriticalPair crown3 (inA i) (inB i).
+Proof.
+  intro i. constructor.
+  - (* incomparable: ~ (crown3 (inA i)(inB i) \/ crown3 (inB i)(inA i)) *)
+    unfold crown3, compose_le, crownF. intros [H|H]; [ exact (H eq_refl) | exact H ].
+  - (* down: vacuous *)
+    intros a [Hle Hne]. destruct a as [a'|b']; simpl in Hle.
+    + subst a'. exfalso; apply Hne; reflexivity.
+    + contradiction.
+  - (* up: vacuous *)
+    intros b [Hle Hne]. destruct b as [a'|b']; simpl in Hle.
+    + contradiction.
+    + subst b'. exfalso; apply Hne; reflexivity.
+Qed.
+
+Lemma crown_alt_cycle : forall i j : Fin.t 3, i <> j ->
+  IsAlternatingCycle crown3 [(inA i, inB i); (inA j, inB j)].
+Proof.
+  intros i j Hij. unfold IsAlternatingCycle. split.
+  - intros p [<-|[<-|[]]]; simpl; apply crown_critical.
+  - simpl. split; [ exact (fun H => Hij (eq_sym H)) | exact Hij ].
+Qed.
+
+Theorem crown3_dim_ge_3 : forall d, PosetDimension crown3 d -> 3 <= d.
+Proof.
+  intros d Hdim.
+  set (r := dimension_realizer Hdim).
+  pose proof (dimension_is_realizer Hdim) as HR.
+  pose proof (dimension_cardinality Hdim) as Hcard.
+  set (E := Carrier (Fin.t 3) (Fin.t 3)).
+  (* ---- r is inhabited ---- *)
+  assert (Hinhab : Inhabited (E -> E -> Prop) r).
+  { destruct (classic (forall L, In (E -> E -> Prop) r L -> L (inA c0) (inB c0)))
+      as [Hall | Hnall].
+    - (* if every L put inA c0 <= inB c0 then crown3 would hold; but it's False *)
+      exfalso.
+      assert (Hfalse : crown3 (inA c0) (inB c0)).
+      { apply (proj2 (realizer_intersection HR (inA c0) (inB c0))). exact Hall. }
+      unfold crown3, compose_le, crownF in Hfalse. exact (Hfalse eq_refl).
+    - apply not_all_ex_not in Hnall. destruct Hnall as [L HL].
+      apply imply_to_and in HL. destruct HL as [HLin _].
+      exact (Inhabited_intro _ _ L HLin). }
+  (* ---- per-pair reverser ---- *)
+  pose proof (proj1 (critical_pair_realizer_iff crown3 crown_carrier_finite
+                       r Hinhab (realizer_linear HR)) HR) as Hrev.
+  destruct (Hrev (inA c0) (inB c0) (crown_critical c0)) as [L0 [HL0in HL0rev]].
+  destruct (Hrev (inA c1) (inB c1) (crown_critical c1)) as [L1 [HL1in HL1rev]].
+  destruct (Hrev (inA c2) (inB c2) (crown_critical c2)) as [L2 [HL2in HL2rev]].
+  (* ---- distinctness of L0, L1, L2 ---- *)
+  assert (Hdistinct : forall (i j : Fin.t 3) (Li Lj : E -> E -> Prop),
+            i <> j ->
+            In (E -> E -> Prop) r Li -> Li (inB i) (inA i) ->
+            In (E -> E -> Prop) r Lj -> Lj (inB j) (inA j) ->
+            Li <> Lj).
+  { intros i j Li Lj Hij HLiin HLirev HLjin HLjrev Heq.
+    set (S := fun p : E * E => p = (inA i, inB i) \/ p = (inA j, inB j)).
+    assert (HS : forall p, In (E * E) S p -> IsCriticalPair crown3 (fst p) (snd p)).
+    { intros [x y] [Hp|Hp]; injection Hp as -> ->; simpl; apply crown_critical. }
+    pose proof (proj1 (critical_pairs_reversible_iff_no_alternating_cycle
+                         crown3 S HS)) as Hfwd.
+    apply Hfwd.
+    - exists Li. split.
+      + exact (realizer_linear HR Li HLiin).
+      + intros x y [Hp|Hp]; injection Hp as -> ->.
+        * exact HLirev.
+        * rewrite Heq. exact HLjrev.
+    - exists [(inA i, inB i); (inA j, inB j)]. split.
+      + intros p [<-|[<-|[]]]; [ left | right ]; reflexivity.
+      + exact (crown_alt_cycle i j Hij). }
+  assert (Hc01 : c0 <> c1) by discriminate.
+  assert (Hc02 : c0 <> c2) by discriminate.
+  assert (Hc12 : c1 <> c2) by discriminate.
+  assert (Hne01 : L0 <> L1) by (apply (Hdistinct c0 c1); assumption).
+  assert (Hne02 : L0 <> L2) by (apply (Hdistinct c0 c2); assumption).
+  assert (Hne12 : L1 <> L2) by (apply (Hdistinct c1 c2); assumption).
+  (* ---- pigeonhole: 3 distinct linear extensions => 3 <= d ---- *)
+  set (Three := Add (E -> E -> Prop)
+                  (Add (E -> E -> Prop)
+                     (Add (E -> E -> Prop) (Empty_set (E -> E -> Prop)) L0) L1) L2).
+  assert (HcardThree : cardinal (E -> E -> Prop) Three 3).
+  { unfold Three. apply card_add.
+    - apply card_add.
+      + apply card_add; [ apply card_empty | intro Hb; destruct Hb ].
+      + intro Hb. destruct Hb as [L' Hb | L' Hb].
+        * destruct Hb.
+        * apply Singleton_inv in Hb. apply Hne01; congruence.
+    - intro Hb. destruct Hb as [L' [L'' Hb | L'' Hb] | L' Hb].
+      + destruct Hb.
+      + apply Singleton_inv in Hb. apply Hne02; congruence.
+      + apply Singleton_inv in Hb. apply Hne12; congruence. }
+  assert (HincThree : Included (E -> E -> Prop) Three r).
+  { unfold Three. intros M HM.
+    destruct HM as [M HM2 | M Hb].
+    - destruct HM2 as [M HM1 | M Hb].
+      + destruct HM1 as [M HM0 | M Hb].
+        * destruct HM0.
+        * apply Singleton_inv in Hb; subst; exact HL0in.
+      + apply Singleton_inv in Hb; subst; exact HL1in.
+    - apply Singleton_inv in Hb; subst; exact HL2in. }
+  exact (incl_card_le (E -> E -> Prop) Three r 3 d HcardThree Hcard HincThree).
+Qed.
